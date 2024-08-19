f9e10f81e5ec135e03c2700fc57586652f3ab79a
Александр Кунцевич <oleksandr@MacBook-Pro-Aleksandr.local>
2024-08-19 12:49:37 +0300

    Initialize and configure Playwright

---
+++ playwright.config.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 2,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10000,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+  reporter: [['list'], ['html', { outputFolder: 'test-results' }]],
+});
